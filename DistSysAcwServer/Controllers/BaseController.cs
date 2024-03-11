4a66a69e5d61f169b816939fd3925ac1595a2b73
KP125689 <P.RWEHUMBIZA-2021@hull.ac.uk>
2024-03-11 19:49:41 +0000

    Add project files.

---
+++ DistSysAcwServer/Controllers/BaseController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DistSysAcwServer.Controllers
+{
+    [Route("api/[Controller]/[Action]")]
+    [ApiController]
+    public abstract class BaseController : ControllerBase
+    {
+        /// <summary>
+        /// This DbContext contains the database context defined in UserContext.cs
+        /// You can use it inside your controllers to perform database CRUD functionality
+        /// </summary>
+        protected Models.UserContext DbContext { get; set; }
+        /// <summary>
+        /// Abstract base controller containing a dependency-injected scoped DbContext (UserContext).
+        /// Inherit from this abstract base to access the common DbContext in a controller.
+        /// </summary>
+        /// <param name="dbcontext">Dependency-injected DbContext</param>
+        public BaseController(Models.UserContext dbcontext)
+        {
+            DbContext = dbcontext;
+        }
+    }
+}
