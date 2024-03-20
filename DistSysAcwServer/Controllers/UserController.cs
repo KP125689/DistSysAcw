7ff0d3a6d60ac0aa70aa35fc3f08abd4311c8c22
KP125689 <P.RWEHUMBIZA-2021@hull.ac.uk>
2024-03-20 19:39:51 +0000

    re-did task4

--- DistSysAcwServer/Controllers/UserController.cs
+++ DistSysAcwServer/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DistSysAcwServer.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 
@@ -9,19 +10,59 @@ namespace DistSysAcwServer.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly UserContext _dbContext;
+
+        public UserController(UserContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         [HttpGet("New")]
         public IActionResult NewGet(string username)
         {
-            // Logic for handling GET request with parameter from URI
-            return Ok($"Creating user '{username}' via GET request.");
+            if (string.IsNullOrEmpty(username))
+            {
+                return Ok("False - User Does Not Exist! Did you mean to do a POST to create a new user?");
+            }
+
+            var userExists = _dbContext.Users.Any(u => u.UserName == username);
+            if (userExists)
+            {
+                return Ok("True - User Does Exist! Did you mean to do a POST to create a new user?");
+            }
+            else
+            {
+                return Ok("False - User Does Not Exist! Did you mean to do a POST to create a new user?");
+            }
         }
 
         [HttpPost("New")]
         public IActionResult NewPost([FromBody] string username)
         {
-            // Logic for handling POST request with JSON string parameter from the body
-            return Ok($"Creating user '{username}' via POST request.");
+            if (string.IsNullOrEmpty(username))
+            {
+                return BadRequest("Oops. Make sure your body contains a string with your username and your Content-Type is Content-Type:application/json");
+            }
+
+            var existingUser = _dbContext.Users.FirstOrDefault(u => u.UserName == username);
+            if (existingUser != null)
+            {
+                return StatusCode(403, "Oops. This username is already in use. Please try again with a new username.");
+            }
+
+            var user = new User
+            {
+                UserName = username,
+                ApiKey = Guid.NewGuid().ToString(),
+                Role = _dbContext.Users.Any() ? "User" : "Admin" // If it's the first user, set as Admin
+            };
+
+            _dbContext.Users.Add(user);
+            _dbContext.SaveChanges();
+
+            return Ok(user.ApiKey);
         }
     }
+        
 }
 #endregion
